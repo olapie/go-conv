fa7034936646f2227420064b0acc7a2556fb0b4e
yxz6 <yxzbox@gmail.com>
2022-11-14 21:38:06 -0500

    Convert slice

--- slice.go
+++ slice.go
@@ -1,1 +1,23 @@
 package conv
+
+import "fmt"
+
+func MustSlice[A any, B any](a []A, f func(A) B) []B {
+	b := make([]B, len(a))
+	for i := range a {
+		b[i] = f(a[i])
+	}
+	return b
+}
+
+func Slice[A any, B any](a []A, f func(A) (B, error)) ([]B, error) {
+	b := make([]B, len(a))
+	var err error
+	for i := range a {
+		b[i], err = f(a[i])
+		if err != nil {
+			return nil, fmt.Errorf("index %d: %w", i, err)
+		}
+	}
+	return b, nil
+}
