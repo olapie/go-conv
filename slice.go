40a247fd7bcab5f2bcbc527e8e1ccd21b448b391
yxz6 <yxzbox@gmail.com>
2022-11-14 21:58:04 -0500

    Convert slice

--- slice.go
+++ slice.go
@@ -21,3 +21,31 @@ func Slice[A any, B any](a []A, f func(A) (B, error)) ([]B, error) {
 	}
 	return b, nil
 }
+
+func MustSliceToSet[A any, B comparable](a []A, f func(A) B) map[B]bool {
+	m := make(map[B]bool, len(a))
+	for _, v := range a {
+		if f == nil {
+			m[any(a).(B)] = true
+		} else {
+			m[f(v)] = true
+		}
+	}
+	return m
+}
+
+func SliceToSet[A any, B comparable](a []A, f func(A) (B, error)) (map[B]bool, error) {
+	m := make(map[B]bool, len(a))
+	for i, v := range a {
+		if f == nil {
+			m[any(a).(B)] = true
+		} else {
+			b, err := f(v)
+			if err != nil {
+				return nil, fmt.Errorf("index %d: %w", i, err)
+			}
+			m[b] = true
+		}
+	}
+	return m, nil
+}
